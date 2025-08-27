1ba2ca73abc280ace0bd2d8097f1ea6559e7cfac
uept42-user <uept42-user@aluno.pr.senac.br>
2025-08-27 09:43:46 -0300

    Declara um mapa que armazena taxas de conversão em moedas

--- app/src/main/java/com/example/projectcurrencyconverterapi/ExchangeRatesResponse.java
+++ app/src/main/java/com/example/projectcurrencyconverterapi/ExchangeRatesResponse.java
@@ -1,5 +1,16 @@
 //Modelo de dados (DTO - Data Transfer Object) que representa a resposta JSON da API
 package com.example.projectcurrencyconverterapi;
-
+//importa a classe Map da bibilioteca Java, usada para armazenar pares de chave-valor
+import java.util.HashMap;
+import java.util.Map;
 public class ExchangeRatesResponse {
+//declara um mapa que armazena taxas de conversão em moedas
+//a chave (string) representa o código  da moeda ex: ("USD", "EUR")
+//e o valor (Double) representa a taxa de conversão para essa moeda
+private Map<String, Double> conversion_rates = new HashMap<>();
+//Metodo publico para acessar as taxas de conversão
+//Retorna o mapa contendo as taxas de conversão armazenadas
+    public Map<String, Double> getConversionRates(){
+        return conversion_rates;
+    }
 }
